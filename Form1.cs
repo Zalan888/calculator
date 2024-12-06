dc1c6b04fb8f18c118f36966f4482d0fe802da71
Zalan888 <witmannz@kkszki.hu>
2024-12-06 12:38:36 +0100

    basics of a calculator, nums, basic operators, delete, and last ANS added

---
+++ Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AngouriMath;
+using AngouriMath.Extensions;
+
+namespace teszt1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static float ans;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 2;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 3;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 4;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 5;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 6;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 7;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 8;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 9;
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 0;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += '-';
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += '*';
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += '/';
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += '+';
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            ans = (float)textBox1.Text.EvalNumerical();
+            textBox1.Text = Convert.ToString((decimal)textBox1.Text.EvalNumerical());
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += Convert.ToString(ans);
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1);
+        }
+    }
+}
